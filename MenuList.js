2c3f60afad20eaf4831cd422d2cbbc69f6e1a820
Sehaj-0902 <101385536+Sehaj-0902@users.noreply.github.com>
2024-12-16 01:29:29 +0530

    helpers

---
+++ MenuList.js
@@ -0,0 +1,39 @@
+import Margherita from '../assets/margherita-pizza.jpg'
+import PaneerTikka from '../assets/paneer-tikka-pizza.png'
+import GardenDelight from '../assets/garden-delight-pizza.jpg'
+import CheeseCorn from '../assets/cheese-corn-pizza.jpg'
+import TandooriChicken from '../assets/tandoori-chicken-pizza.jpg'
+import Pepperoni from '../assets/pepperoni-pizza.jpg'
+
+export const MenuList = [
+    {
+        name: 'Margherita',
+        image: Margherita,
+        price: 249,
+    },
+    {
+        name: 'Paneer Tikka',
+        image: PaneerTikka,
+        price: 349,
+    },
+    {
+        name: 'Garden Delight',
+        image: GardenDelight,
+        price: 299,
+    },
+    {
+        name: 'Cheese n Corn',
+        image: CheeseCorn,
+        price: 249,
+    },
+    {
+        name: 'Tandoori Chicken',
+        image: TandooriChicken,
+        price: 399,
+    },
+    {
+        name: 'Pepperoni',
+        image: Pepperoni,
+        price: 349,
+    },
+];
